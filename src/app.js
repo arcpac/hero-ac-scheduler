bf2c42ed266803aecd40c28a14fb65a4c9f6efc4
antoncab <antonraphaelcaballes@gmail.com>
2024-01-15 17:44:20 +1100

    point to public root

--- src/app.js
+++ src/app.js
@@ -33,13 +33,6 @@ app.use((req, res, next) => {
 app.use(helmet())
 app.use(express.json());
 // ===
-app.use(express.static(path.join(__dirname, "frontend", "build")))
-
-// ...
-// Right before your app.listen(), add this:
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "..", "frontend", "build", "index.html"));
-});
 
 app.use("/", login)
 app.use("/forgot-password", auth)
--- src/server.js
+++ src/server.js
@@ -1,6 +1,18 @@
 const http = require("http");
 const app = require("./app");
+const path = require("path")
+
+const express = require("express");
 const { mongoConnect } = require("../src/services/mongo")
+
+app.use(express.static(path.join(__dirname, "..", "public")))
+
+// ...
+// Right before your app.listen(), add this:
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "frontend", "build", "index.html"));
+});
+
 const server = http.createServer(app);
 
 
