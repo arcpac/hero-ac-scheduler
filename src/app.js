f85af7fd7c2c3d70df296ae6bb6fef5bde91b225
antoncab <antonraphaelcaballes@gmail.com>
2024-01-15 16:47:41 +1100

    change path.join

--- src/app.js
+++ src/app.js
@@ -33,7 +33,7 @@ app.use((req, res, next) => {
 app.use(helmet())
 app.use(express.json());
 // ===
-app.use(express.static(path.join(__dirname, "client", "build")))
+app.use(express.static(path.join(__dirname, "frontend", "build")))
 
 // ...
 // Right before your app.listen(), add this:
