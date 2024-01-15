9775c297d8df3b6cda4b855b9ff75824f2a793ac
antoncab <antonraphaelcaballes@gmail.com>
2024-01-15 18:14:15 +1100

    change to public

--- src/server.js
+++ src/server.js
@@ -5,12 +5,15 @@ const path = require("path")
 const express = require("express");
 const { mongoConnect } = require("../src/services/mongo")
 
+
+app.use(express.json());
+
 app.use(express.static(path.join(__dirname, "..", "frontend", "build")))
 
 // ...
 // Right before your app.listen(), add this:
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "..", "frontend", "build", "index.html"));
+  res.sendFile(path.join(__dirname, "..", "frontend", "public", "index.html"));
 });
 
 const server = http.createServer(app);
