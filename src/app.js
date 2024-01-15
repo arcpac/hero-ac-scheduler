784a854e41bc48dae5c5cc237146812e78c12569
antoncab <antonraphaelcaballes@gmail.com>
2024-01-15 17:19:38 +1100

    fix path in app.js

--- src/app.js
+++ src/app.js
@@ -38,7 +38,7 @@ app.use(express.static(path.join(__dirname, "frontend", "build")))
 // ...
 // Right before your app.listen(), add this:
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
+  res.sendFile(path.join(__dirname, "..", "frontend", "build", "index.html"));
 });
 
 app.use("/", login)
